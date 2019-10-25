14440473ac29efdba8503ad7dd92fe82bdaa27f8
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-10-25 22:19:48 +0300

    Dynamic displaying logo added

--- wp-content/themes/law/header.php
+++ wp-content/themes/law/header.php
@@ -45,33 +45,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xs-2">
-                        <div id="fh5co-logo"><a href="index.html">Law<span>.</span></a></div>
+                        <div id="fh5co-logo"><a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a></div>
                     </div>
                         <?php wp_nav_menu( array(
                             'theme_location' => 'header-menu',
                             'container_class' => 'col-xs-10 text-right menu-1',
                             'walker' => new Law_Header_Menu_Walker
                         ) ); ?>
-<!--                    <div class="col-xs-10 text-right menu-1">-->
-<!--                        <ul>-->
-<!--                            <li class="active"><a href="index.html">Home</a></li>-->
-<!--                            <li><a href="practice.html">Practice Areas</a></li>-->
-<!--                            <li><a href="won.html">Won Cases</a></li>-->
-<!--                            <li class="has-dropdown">-->
-<!--                                <a href="blog.html">Blog</a>-->
-<!--                                <ul class="dropdown">-->
-<!--                                    <li><a href="#">Web Design</a></li>-->
-<!--                                    <li><a href="#">eCommerce</a></li>-->
-<!--                                    <li><a href="#">Branding</a></li>-->
-<!--                                    <li><a href="#">API</a></li>-->
-<!--                                </ul>-->
-<!--                            </li>-->
-<!--                            <li><a href="about.html">About</a></li>-->
-<!--                            <li><a href="contact.html">Contact</a></li>-->
-<!--                            <li class="btn-cta"><a href="#"><span>Login</span></a></li>-->
-<!--                            <li class="btn-cta"><a href="#"><span>Sign Up</span></a></li>-->
-<!--                        </ul>-->
-<!--                    </div>-->
                 </div>
 
             </div>
